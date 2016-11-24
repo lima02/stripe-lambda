dd89b01408cb97787f480f61e4d40ca6337291b0
lima02 <mig.c.lima@gmail.com>
2016-11-24 19:14:06 +1100

    Lambda Proxy Integration approach

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require('dotenv').load();
 
 var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = function(event, context) {
-
+exports.handler = function(event, context, callback) {
+  console.log('v0.9.2');
+  console.log(event.body);
   var requestBody = JSON.parse(event.body);
 
   stripe.charges.create({
@@ -12,12 +13,12 @@ exports.handler = function(event, context) {
     currency:      requestBody.currency || 'aud',
     description:   requestBody.description || 'Stripe payment ' + requestBody.source
   }, function(err, charge) {
-      if (err) {
-        context.fail(JSON.stringify(err));
-      }
-      else {
-        context.succeed(JSON.stringify(charge));
-      }
-    }
-  );
+      callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(charge),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+  });
 }
